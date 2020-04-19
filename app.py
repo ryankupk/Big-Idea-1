548ca468d39ef436eefb59c636957b81de103711
Fredo Hausner <fredohausner@Fredos-MacBook-Pro.local>
2020-04-19 15:26:58 -0600

    live reload

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 #flask app
 import flask
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def mainRoute():
     return "hello world"
+
+@app.route('/testing', methods=['POST'])
+def testing():
+    data = request.data
+    print(data)
+    return {
+        'testing': 'something hm'
+    }
+
+@app.route('/something')
+def something():
+    return {
+        'something': 'something'
+    }
 
 
 app.run(debug=True)
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source ./flask/bin/activate
+export FLASK_DEBUG=1
 flask run
