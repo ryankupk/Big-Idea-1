0f889f06308192aecffec0ed791ac1772c4e37fd
Fredo Hausner <fredohausner@Fredos-MacBook-Pro.local>
2020-04-20 20:04:31 -0600

    Changed start script

--- start.sh
+++ start.sh
@@ -1,13 +1,4 @@
-#flask app
-import flask
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/')
-def mainRoute():
-    return "hello world"
-
-app.run(debug=True)
+#!/bin/bash
+source ./flask/bin/activate
+export FLASK_DEBUG=1
+python3 app.py
