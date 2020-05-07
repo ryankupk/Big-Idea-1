f31d2645f3c97e509eeaa3c0c12c7705a371b899
Fredo Hausner <fredohausner@Fredos-MacBook-Pro.local>
2020-05-06 23:00:23 -0600

    cors and something idk

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var http = require("http").createServer(app);
 var bodyParser = require("body-parser");
 var io = require('socket.io')(http, { origins: '*:*'});
 var db = require('./db.js');
+var cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 
 app.get('/', (req, res)=>{
@@ -17,37 +19,41 @@ app.get('/', (req, res)=>{
 
 
 app.post('/newuser', (req, res) =>{
-  var command = 'SELECT COUNT(*) AS isMatch FROM Users WHERE Username=?';
+  console.log(req.body);
+  res.send({
+    hello: 'world'
+  });
+  // var command = 'SELECT COUNT(*) AS isMatch FROM Users WHERE Username=?';
 
-  db.query(command, [req.body.username], (error, results) =>{
-    if(error) {
-      console.log(error)
-      res.send({'error': 'dbFailure'});
-    }
+  // db.query(command, [req.body.username], (error, results) =>{
+  //   if(error) {
+  //     console.log(error)
+  //     res.send({'error': 'dbFailure'});
+  //   }
 
-    else if(results[0].isMatch) res.send({'error': 'usernameExists'});
+  //   else if(results[0].isMatch) res.send({'error': 'usernameExists'});
 
-    else{
-      command = 'INSERT INTO Users (Username, Password) VALUES (?, ?)'
-      db.query(command, [req.body.username, req.body.password], (error) => {
-        if(error){
-          console.log(error)
-          res.send({'error': 'dbFailure'});
-        }
+  //   else{
+  //     command = 'INSERT INTO Users (Username, Password) VALUES (?, ?)'
+  //     db.query(command, [req.body.username, req.body.password], (error) => {
+  //       if(error){
+  //         console.log(error)
+  //         res.send({'error': 'dbFailure'});
+  //       }
 
-        else res.send({'success': 'userCreated'})
+  //       else res.send({'success': 'userCreated'})
 
-      })
-      db.commit();
-    }
-  })
+  //     })
+  //     db.commit();
+  //   }
+  // })
 });
 
 
-app.post('/authenticate', (req, res) =>{
+app.post('/authenticate', (req, res) => {
   var command = 'SELECT COUNT(*) AS isMatch FROM Users WHERE Username=? AND Password=?';
 
-  db.query(command, [req.body.username, req.body.password], (error, results) =>{
+  db.query(command, [req.body.username, req.body.password], (error, results) => {
     if(error){
       console.log(error)
       res.send({'error': 'dbFailure'});
