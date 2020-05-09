906d73366604633b243bba4b1334254377c05556
Fredo Hausner <fredohausner@Fredos-MacBook-Pro.local>
2020-05-09 01:53:03 -0600

    checking db to see if user already exists, then adding user if it doesnt, added status codes for front end

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 "use strict";
-var express = require("express");
-var app = express();
-var fs = require("fs");
-var http = require("http").createServer(app);
-var bodyParser = require("body-parser");
-var io = require('socket.io')(http, { origins: '*:*'});
-var db = require('./db.js');
-var cors = require('cors');
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const http = require("http").createServer(app);
+const bodyParser = require("body-parser");
+const io = require('socket.io')(http, { origins: '*:*'});
+const db = require('./db.js');
+const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,69 +18,74 @@ app.get('/', (req, res)=>{
 });
 
 
-app.post('/newuser', (req, res) =>{
+app.post('/createUser', (req, res) => {
+  console.log(req.body);
+
+  let searchUser = 'Select * from Users where Username = ?'
+  db.query(searchUser, [req.body.email], (err, results) => {
+    if(err){
+      res.status(400).send({
+        message: err.message
+      });
+    } else {
+      if(results.length > 0){
+        res.status(400).send({
+          message: 'User already exists'
+        });
+      } else {
+        let createUser = 'Insert into Users (UserID, Username, Password) values (?,?,?)';
+        let timestamp = Date.now();
+        db.query(createUser, [timestamp, req.body.email, req.body.password], (err, results) => {
+          if(err){
+            res.status(400).send({
+              message: err.message
+            });
+          } else {
+            res.send({
+              message: 'User Succesfully Created.'
+            });
+          };
+        });
+      };
+    };
+  });
+});
+
+app.post('/authenticate', (req, res) => {
   console.log(req.body);
   res.send({
-    hello: 'world'
+    'type': 'authenticate'
   });
-  // var command = 'SELECT COUNT(*) AS isMatch FROM Users WHERE Username=?';
+  // var command = 'SELECT COUNT(*) AS isMatch FROM Users WHERE Username=? AND Password=?';
 
-  // db.query(command, [req.body.username], (error, results) =>{
-  //   if(error) {
+  // db.query(command, [req.body.username, req.body.password], (error, results) => {
+  //   if(error){
   //     console.log(error)
   //     res.send({'error': 'dbFailure'});
   //   }
-
-  //   else if(results[0].isMatch) res.send({'error': 'usernameExists'});
-
+  //   else if(results[0].isMatch){
+  //     res.send({'authenticated': 1})
+  //   }
   //   else{
-  //     command = 'INSERT INTO Users (Username, Password) VALUES (?, ?)'
-  //     db.query(command, [req.body.username, req.body.password], (error) => {
-  //       if(error){
-  //         console.log(error)
-  //         res.send({'error': 'dbFailure'});
-  //       }
-
-  //       else res.send({'success': 'userCreated'})
-
-  //     })
-  //     db.commit();
+  //     res.send({'authenticated': 0})
   //   }
   // })
 });
 
 
-app.post('/authenticate', (req, res) => {
-  var command = 'SELECT COUNT(*) AS isMatch FROM Users WHERE Username=? AND Password=?';
+// io.on("db", function (socket) {
+//     console.log('user connected');
 
-  db.query(command, [req.body.username, req.body.password], (error, results) => {
-    if(error){
-      console.log(error)
-      res.send({'error': 'dbFailure'});
-    }
-    else if(results[0].isMatch){
-      res.send({'authenticated': 1})
-    }
-    else{
-      res.send({'authenticated': 0})
-    }
-  })
-});
+//     socket.on("disconnect", function () {
+//       console.log('disconnected');
+//     });
 
-
-io.on("db", function (socket) {
-    console.log('user connected');
-
-    socket.on("disconnect", function () {
-      console.log('disconnected');
-    });
-
-    socket.on('message', (msg)=>{
-        console.log(msg);
-        // socket.broadcast.send('message', msg);
-        socket.broadcast.emit('message', msg);
-    });
-  });
+//     socket.on('message', (msg)=>{
+//         console.log(msg);
+//         // socket.broadcast.send('message', msg);
+//         socket.broadcast.emit('message', msg);
+//     });
+//   });
 
 http.listen(3000, function () {
     console.log("listening on port: 3000");
